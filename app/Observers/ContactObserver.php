a58daaf6818be4c70f2e69ad96a864231ab7f91b
datikken <tikken23@gmail.com>
2021-03-06 22:51:05 +0300

    contacts observer tg message

---
+++ app/Observers/ContactObserver.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Contact;
+use Illuminate\Support\Facades\Log;
+use NotificationChannels\Telegram\TelegramMessage;
+use Illuminate\Support\Facades\Log;
+
+class ContactObserver
+{
+    /**
+     * Handle the Contact "created" event.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return void
+     */
+    public function created(Contact $contact)
+    {
+        return TelegramMessage::create()
+            // Optional recipient user id.
+            ->to(1)
+            // Markdown supported.
+            ->content("Hello there!\nYour invoice has been *PAID*");
+
+
+        Log::info(['new_contact_added' => $contact]);
+    }
+
+    /**
+     * Handle the Contact "updated" event.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return void
+     */
+    public function updated(Contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Handle the Contact "deleted" event.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return void
+     */
+    public function deleted(Contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Handle the Contact "restored" event.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return void
+     */
+    public function restored(Contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Handle the Contact "force deleted" event.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return void
+     */
+    public function forceDeleted(Contact $contact)
+    {
+        //
+    }
+}
--- config/services.php
+++ config/services.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Third Party Services
@@ -13,7 +12,9 @@ return [
     | a conventional file to locate the various service credentials.
     |
     */
-
+    'telegram-bot-api' => [
+        'token' => env('TELEGRAM_BOT_TOKEN', '1598375531:AAGo2wE1BXm38tKbipBc3AqKsfrdUNftH4k')
+    ],
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
